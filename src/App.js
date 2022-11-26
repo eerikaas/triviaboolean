a2280c10f0406673e09cd9eacfc39b6949fe5190
eerika <eelehtisyrja@hotmail.com>
2022-11-26 12:11:01 +0200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getNextKeyDef } from '@testing-library/user-event/dist/keyboard/getNextKeyDef';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const [insert, setInsert] = useState([])
+const [correct, setCorrect] = useState()
+const [points, setPoints] = useState(0)
+
+useEffect(() => {
+  next()
+}, [])
+
+const next = () => {
+  fetch('https://opentdb.com/api.php?amount=1&type=boolean')
+  .then(response => response.json())
+
+  .then(resData => {
+    setCorrect(null)
+    setInsert(resData.results[0])
+  })
+  .catch(err => console.log(err))
+}
+
+const handleClick = (e, value) => {
+  e.preventDefault();
+  if (e.target.value === insert.correct_answer) {
+    pointCount()
+    next()
+  } else {
+    next()
+  }
+}
+
+const pointCount = () => {
+  setPoints(points + 1)
+}
+
+return (
+  <div>
+    <p className='Game'>{insert.question}</p>
+      <button onClick={handleClick} value="True" className='Button'>True</button>
+      <button onClick={handleClick} value="False" className='Button'>False</button>
+      <button onClick={next} className='Button'>Skip question</button>
+      {points > 0 ? <p>Your points: {points}</p> : null}
+  </div>
+)
 }
 
 export default App;
