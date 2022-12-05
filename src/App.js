1451ce1bad612f882f9378443d41ab0acc22204e
eerika <eelehtisyrja@hotmail.com>
2022-12-05 14:40:31 +0200

    new update

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from "react";
 import "./App.css";
 import Life from "./lives/Life.js";
 import NoLife from "./lives/NoLife.js";
+import header from "./header.png";
 
 window.addEventListener("animationend", (event) => {
   event.target.className = "Button"
@@ -10,10 +11,10 @@ window.addEventListener("animationend", (event) => {
 function App() {
   const [insert, setInsert] = useState([])
   const [points, setPoints] = useState(0)
-  const [life, setLife] = useState(5)
+  const [life, setLife] = useState(3)
 
   const next = useCallback(() => {
-    fetch("https://opentdb.com/api.php?amount=1&type=boolean")
+    fetch("https://opentdb.com/api.php?amount=1&difficulty=hard&type=boolean")
       .then((response) => response.json())
       .then((resData) => {
         setInsert(resData.results[0])
@@ -41,7 +42,7 @@ function App() {
 
   const newGame = () => {
     setPoints(0)
-    setLife(5)
+    setLife(3)
     next()
   }
 
@@ -71,21 +72,24 @@ const LifeIcons = ({ count, lost }) => (
 
   if (life > 0) {
     return (
-      <div style={{backgroundImage: "url('https://img.freepik.com/free-vector/cute-rainbow-frame-vector-beige-background-cute-hand-drawn-style_53876-126112.jpg?w=2000')", height:"100vh"}}>
+      <div>
+      <img src={header} className="center-image"/>
+      <div>
         <p style={{fontSize: "40px"}} className="Game">{decode(insert.question)}</p>
         <button onClick={handleClick} value="True" className="Button">True</button>
         <button onClick={handleClick} value="False" className="Button">False</button>
-        <button onClick={skipper} className="Button">Skip question</button>
-        <LifeIcons count={life} lost={5 - life} />
+        <button onClick={skipper} className="Button">Skip</button>
+        <LifeIcons count={life} lost={3 - life} />
         <h2 style={{textAlign: "center"}}>{points > 0 ? <p>Your points: {points}</p> : null}</h2>
+      </div>
       </div>
     )
   } else {
     return (
       <div style={{backgroundImage: "url('https://wallpaperaccess.com/full/1305831.jpg')", height:"100vh", textAlign: "center" }}>
-        <h1>GAME OVER!</h1>
-        <p>Your score: {points}</p>
-        <button className="Button" onClick={newGame}>Play again :-)</button>
+        <h1 style={{ fontSize: '80px' }}>GAME OVER!</h1>
+        <p style={{ fontSize: '30px' }}>Your score was only {points} points...</p>
+        <button className="Button" onClick={newGame}>Play again? :-)</button>
       </div>
     )
   }
